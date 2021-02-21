c2dad92e64f967faf1b85aa0d312ab16a733ba1d
hannalt <54518647+hannalt@users.noreply.github.com>
2021-02-21 09:20:19 -0500

    Hanna's functions

---
+++ script.py
@@ -0,0 +1,3 @@
+from search import *
+
+find_matches('apple chips')
---
+++ search.py
@@ -0,0 +1,22 @@
+import json
+
+def fix_string(phrase):
+	punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+	for x in phrase.lower(): 
+        	if x in punctuations: 
+            		phrase = phrase.replace(x, "") 
+	return phrase.lower()
+
+
+def find_matches(search_phrase):
+	possibilities = []
+	search_phrase = fix_string(search_phrase)
+	with open ('final_data.jsonl', 'r', encoding = 'utf-8') as data:
+		for line in data:
+			line = json.loads(line)
+			line_search = fix_string(line['product_name'])
+			if search_phrase in line_search:
+				return line
+				#possibilities.append(line)
+	#return possibilities	 
+	
